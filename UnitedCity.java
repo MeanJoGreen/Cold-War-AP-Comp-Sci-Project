805a44cf3e5e65dc7c604f1a3fd18fd990386ff1
MeanJoGreen <gmccauley@springstreet.org>
2016-05-12 20:05:10 -0700

    Create UnitedCity.java

---
+++ UnitedCity.java
@@ -0,0 +1,24 @@
+public class UnitedCity {
+//to return if the city is destroyed or not
+
+   boolean NewYork;
+   boolean LosAngeles;
+   boolean Chicago;
+   boolean Houston;
+   boolean Philidelphia;
+   boolean Phoenix;
+   boolean SanAntonio;
+   boolean SanDiego;
+   boolean Dallas;
+   boolean SanJose;
+   boolean Austin;
+   boolean Jacksonville;
+   boolean SanFrancisco;
+   boolean Indianapolis;
+   boolean Columbus;
+   
+   public NewYork (){
+   
+   }
+}
+      
