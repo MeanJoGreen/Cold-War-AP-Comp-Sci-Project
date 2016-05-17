e2cdad5162ac459c7f89d6edb7a314c06457ea8d
MeanJoGreen <gmccauley@springstreet.org>
2016-05-17 13:12:26 -0700

    Create UnitedHealth.java

---
+++ UnitedHealth.java
@@ -0,0 +1,86 @@
+public class UnitedHealth{
+      private population;
+      
+
+      public void newYork(boolean destroyed){
+         int population = 8491079;
+         destroyed = false;
+      }
+      
+      public void LasAngeles(boolean destroyed){
+         int population = 3928864	;
+         destroyed = false;
+      }
+      public void Chicago(boolean destroyed){
+         int population = 2722389;
+         destroyed = false;
+      }
+      public void Houston(boolean destroyed){
+         int population = 2239558;
+         destroyed = false;
+      }
+      public void Philadelphia(boolean destroyed){
+         int population = 1560297	;
+         destroyed = false;
+      }
+      public void Phoenix(boolean destroyed){
+         int population = 1537058;
+         destroyed = false;
+      }
+      public void San Antonio(boolean destroyed){
+         int population = 1436697;
+         destroyed = false;
+      }
+      public void San Diego(boolean destroyed){
+         int population = 1381069	;
+         destroyed = false;
+      }
+      public void Dallas(boolean destroyed){
+         int population = 1281047;
+         destroyed = false;
+      }
+      public void San Jose(boolean destroyed){
+         int population = 1015785;
+         destroyed = false;
+      }
+      public void Austin(boolean destroyed){
+         int population = 912791;
+         destroyed = false;
+      }
+      public void Jacksonville(boolean){
+         int population = 853382;
+         destroyed = false;
+      }
+      public void San Francisco(boolean){
+         int population = 852469	;
+         destroyed = false;
+      }
+      public void Indianapolis(boolean){
+         int population = 848788	;
+         destroyed = false;
+      }
+      public void Columbus(boolean){
+         int population = 835957;
+         destroyed = false;
+      } 
+      public void Fort Worth(boolean){
+         int population = 812238;
+         destroyed = false;
+      } 
+      public void Charlotte(boolean){
+         int population = 809958;
+         destroyed = false;
+      } 
+      public void Detroit(boolean){
+         int population = 680250;
+         destroyed = false;
+      } 
+      public void El Paso(boolean){
+         int population = 679036;
+         destroyed = false;
+      }     
+      public void Seattle(boolean){
+         int population = 668,342;
+         destroyed = false;
+      }       
+}
