f560b2ac6eb4a4fec49e8b1796b51fc00a2deb9e
MeanJoGreen <gmccauley@springstreet.org>
2016-05-23 13:18:21 -0700

    Create City.java

---
+++ City.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class City {
+   final Random random = new Random();
+
+   public City(String name){
+      boolean destroyed = false;
+      for(int i = 0; i <10; i ++){
+         int nuke = random.nextInt(7);
+         System.out.println(nuke);
+
+      }
+   }
+}
+      
